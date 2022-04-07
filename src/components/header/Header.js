d7a9e53fa4d7f4882286173888a026f5bceea291
Mert keskin <mertkeskinmtk6@gmail.com>
2022-04-07 14:06:30 +0300

    create and done header and global css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
+import HeaderSocials from './HeaderSocials';
+import ME from '../../assets/author.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Mert Keskin</h1>
+        <h5 className='text-light'>Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className='me'>
+          <img src={ME} alt="" />
+        </div>
+        
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.js
@@ -0,0 +1,14 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from 'react';
+import {BsLinkedin, BsGithub} from 'react-icons/bs';
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href="https://linkedin.com" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+      <a href="https://github.com" target="_blank" rel="noreferrer"><BsGithub /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials;
